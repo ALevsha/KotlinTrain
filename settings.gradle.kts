57d41593299c7a24c8371fa219a337f9d131ac35
ALevsha <levsha26r@gmail.com>
2022-07-21 13:38:14 +0300

    added JavaModule

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,4 @@
 
 rootProject.name = "KotlinTrain"
-
+include("src:main:untitled")
+findProject(":src:main:untitled")?.name = "untitled"
---
+++ src/main/java/JavaChoose.java
@@ -0,0 +1,5 @@
+public class JavaChoose {
+    public static void main(String[] args) {
+        System.out.println("Hellow form Java");
+    }
+}
