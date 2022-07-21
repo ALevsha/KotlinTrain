3a5b2114be4580b13e7db28c43d47cd5cb5ed799
ALevsha <levsha26r@gmail.com>
2022-07-21 15:12:07 +0300

    Java/Kotlin разница: типы данных

--- src/main/java/JavaChoose.java
+++ src/main/java/JavaChoose.java
@@ -1,5 +1,38 @@
 public class JavaChoose {
     public static void main(String[] args) {
         System.out.println("Hellow form Java");
+        /*
+        Типы данных
+         */
+
+        // в java примитивные типы не поддерживают null
+        Integer v2 = 10;
+        int value = 5;
+        double sum = 10.5;
+        final String text = "Hello Java";
+        //final - аналог val. Значит, что переменную можно инициализировать только 1 раз
+
+        v2 = null;
+        value = 10;
+
+        if(v2 > 5) // v2 = null -> будет ошибка
+            System.out.println(">5");
+
+        switch (value){
+            // без break продолжится выполнение кода следующего case
+            case 1:{
+
+                break;
+            }
+            case 5:{
+
+                break;
+            }
+            default:{
+
+
+            }
+
+        }
     }
 }
